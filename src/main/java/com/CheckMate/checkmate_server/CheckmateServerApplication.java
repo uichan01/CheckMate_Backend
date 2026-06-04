6e21d70fd408a8d4af92cda9a1c2d518959728c8
정의찬 <uichan02@naver.com>
2026-06-04 15:54:47 +0900

    init

---
+++ src/main/java/com/CheckMate/checkmate_server/CheckmateServerApplication.java
@@ -0,0 +1,13 @@
+package com.CheckMate.checkmate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CheckMate/checkmate_server/CheckmateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CheckMate.checkmate_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
