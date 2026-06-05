195e5a45c11f990dc77367f5d71ce1eeec11933d
정의찬 <uichan02@naver.com>
2026-06-05 09:37:21 +0900

    feat: ApiResponse

---
+++ src/main/java/com/CheckMate/checkmate_server/response/ApiResponse.java
@@ -0,0 +1,25 @@
+package com.CheckMate.checkmate_server.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private int code;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, 200, "OK", data);
+    }
+
+    public static ApiResponse<Void> success() {
+        return new ApiResponse<>(true, 200, "OK", null);
+    }
+
+    public static ApiResponse<Void> error(int code, String message) {
+        return new ApiResponse<>(false, code, message, null);
+    }
+}
