f059414dda49a3378325b4d1ba912d286bc1c391
원찬혁 <wonkgb0922@naver.com>
2026-06-05 15:55:28 +0900

    feat: StduyGroup의  Contoller, Service, Repository, Domain, RequestDto 생성

---
+++ src/main/java/com/CheckMate/checkmate_server/study/group/repository/StudyGroupRepository.java
@@ -0,0 +1,4 @@
+package com.CheckMate.checkmate_server.study.group.repository;
+
+public interface StudyGroupRepository {
+}
---
+++ src/main/java/com/CheckMate/checkmate_server/study/group/service/StudyGroupService.java
@@ -0,0 +1,8 @@
+package com.CheckMate.checkmate_server.study.group.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudyGroupService {
+
+}
