8f1449f798c8f5834029c9aed2243ae0116d1910
정의찬 <uichan02@naver.com>
2026-06-23 00:55:43 +0900

    fix: StudyGroupController 사용안하는 임포트 제거

--- src/main/java/com/CheckMate/checkmate_server/study/group/controller/StudyGroupController.java
+++ src/main/java/com/CheckMate/checkmate_server/study/group/controller/StudyGroupController.java
@@ -5,11 +5,8 @@ import com.CheckMate.checkmate_server.study.group.dto.req.StudyGroupRequestDto;
 import com.CheckMate.checkmate_server.study.group.dto.req.StudyGroupSearchRequest;
 import com.CheckMate.checkmate_server.study.group.dto.res.StudyGroupResponseDto;
 import com.CheckMate.checkmate_server.study.group.service.StudyGroupService;
-import com.CheckMate.checkmate_server.user.dto.res.SignUpResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
